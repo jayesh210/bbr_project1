bc1548865ecda579f9b547ea667dbb339b59ae76
Jayesh Paliwal <56557919+jayesh210@users.noreply.github.com>
2020-08-10 21:32:07 +0530

    Update javasc.js

--- javasc.js
+++ javasc.js
@@ -1,11 +1,11 @@
 
-$(".btn-canc").hide(1000);
+$(".btn-coll").hide(1000);
 
 $(".btn-coll").click(function(){
      $("ul").hide(1000);
      $(".bbr").hide(1000);
-    $(".btn-coll").hide(1000);
-    $(".btn-canc").show(1000);
+    $(".btn-coll").show(1000);
+    $(".btn-canc").hide(1000);
 
    
     $(".sidebar").addClass("side-adj"); });
@@ -14,8 +14,9 @@ $(".btn-coll").click(function(){
    $(".btn-canc").click(function(){
      $("ul").show(1000);
      $(".bbr").show(1000);
-    $(".btn-coll").show(1000);
-    $(".btn-canc").hide(1000);
+          $(".btn-coll").hide(1000);
+    $(".btn-canc").show(1000);
+  
        $(".sidebar").removeClass("side-adj");
      
    });
