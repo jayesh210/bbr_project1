6e579cff882003ea3884e8a354f182db0392ed2e
Jayesh Paliwal <56557919+jayesh210@users.noreply.github.com>
2020-08-10 12:08:47 +0530

    Add files via upload

---
+++ javasc.js
@@ -0,0 +1,33 @@
+////alert("working")
+//$(".btn-coll").cli(function(){
+//    alert("working");
+//    button.innerHTML("<i class="fas fa-times"></i>");
+//    
+//});
+//}
+$(".btn-canc").hide(1000);
+alert("hi ");
+$(".btn-coll").click(function(){
+     $("ul").hide(1000);
+     $(".bbr").hide(1000);
+    $(".btn-coll").hide(1000);
+    $(".btn-canc").show(1000);
+
+   
+    $(".sidebar").addClass("side-adj"); });
+
+
+   $(".btn-canc").click(function(){
+     $("ul").show(1000);
+     $(".bbr").show(1000);
+    $(".btn-coll").show(1000);
+    $(".btn-canc").hide(1000);
+       $(".sidebar").removeClass("side-adj");
+     
+   });
+    
+    
+    
+   
+
+
